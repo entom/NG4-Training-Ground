e2e901302b77b4c587d90e6cbc68da48d5ab1b72
Tomasz Nicieja <tomasznicieja@gmail.com>
2017-08-18 22:50:23 +0200

    changed app to heroes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {Hero} from './hero';
-import {HeroService} from './hero.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [HeroService]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
-    constructor(private heroService: HeroService) { }
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+  constructor() { }
 
-    ngOnInit(): void {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    }
+  ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
